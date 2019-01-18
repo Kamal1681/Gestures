e5c1a8c2f03d56952a5e040e91c2b8bdfb485131
Kamal Maged <Kamal_Maged@yahoo.com>
2019-01-17 19:21:17 -0500

    Initial Commit

---
+++ Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gestures
+//
+//  Created by Kamal Maged on 2019-01-17.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
